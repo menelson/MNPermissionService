701d00f61ee7429a0fd8d000b816d43da23ba9e1
Mike Nelson <miken@vmware.com>
2017-06-29 08:44:03 -0400

    Create Xcode project

---
+++ MNPermissionService/MNPermissionService/MNPermissionService.h
@@ -0,0 +1,19 @@
+//
+//  MNPermissionService.h
+//  MNPermissionService
+//
+//  Created by Mike Nelson on 6/29/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MNPermissionService.
+FOUNDATION_EXPORT double MNPermissionServiceVersionNumber;
+
+//! Project version string for MNPermissionService.
+FOUNDATION_EXPORT const unsigned char MNPermissionServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MNPermissionService/PublicHeader.h>
+
+
